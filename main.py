6c7faae438006f7b4eac13204b2c823d3e2d074b
Goncalvs98 <52613723+Goncalvs98@users.noreply.github.com>
2024-08-12 15:39:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,27 +16,38 @@ def get_db_connection():
 
 # Rota para obter todos os cargos
 @app.route('/get_medicamentos', methods=['GET'])
-def get_cargos():
-    connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute("select c.descricao as Categoria, mt.descricao as Motivo, nome, codigo, quantidade_minima, localizacao from rm93069.medicamentos m join rm93069.categorias c on m.id_categoria = c.id_categoria join rm93069.motivos mt on m.id_motivo = mt.id_motivo")
-    rows = cursor.fetchall()  # Obtemos todas as linhas da tabela
+def get_medicamentos():
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute(
+            "SELECT c.descricao AS Categoria, mt.descricao AS Motivo, nome, codigo, quantidade_minima, localizacao, s.descricao AS Status "
+            "FROM rm93069.medicamentos m "
+            "JOIN rm93069.categorias c ON m.id_categoria = c.id_categoria "
+            "JOIN rm93069.motivos mt ON m.id_motivo = mt.id_motivo "
+            "JOIN rm93069.status_medicamento sm ON m.id_medicamento = sm.id_medicamento "
+            "JOIN rm93069.status s ON sm.id_status = s.id_status")
+        rows = cursor.fetchall()
 
-    cargos = []
-    for row in rows:
-        cargo = {
-            "Categoria": row[0],
-            "Motivo": row[1],
-            "Nome": row[2],
-            "Código": row[3],
-            "Quantidade Minima": row[4],
-            "Localização": row[5],
-        }
-        cargos.append(cargo)
+        cargos = []
+        for row in rows:
+            cargo = {
+                "Categoria": row[0],
+                "Motivo": row[1],
+                "Nome": row[2],
+                "Código": row[3],
+                "Quantidade Minima": row[4],
+                "Localização": row[5],
+                "Status": row[6],
+            }
+            cargos.append(cargo)
 
-    cursor.close()
-    connection.close()
-    return jsonify(cargos)  # Retorna a lista de cargos em formato JSON
+        cursor.close()
+        connection.close()
+        return jsonify(cargos)
+
+    except oracledb.DatabaseError as e:
+        return jsonify({"error": str(e)}), 500
 
 # Ponto de entrada da aplicação
 if __name__ == '__main__':
